25940cb12515c8ab5d90006901734a0258b3c0ac
Joshua Bitton <joshtosh13@hotmail.com>
2025-10-18 15:39:27 -0400

    Query for 3.3
    Added the query for solving 3.3 as well as the result

---
+++ queries/3.3.sql
@@ -0,0 +1,8 @@
+USE HospitalSystem;
+-- SELECT TOP 10 * FROM Patients;
+-- SELECT TOP 10 * FROM Admissions;
+
+SELECT p.patient_id, COUNT(a.admission_id) AS AdmissionCount
+FROM Patients p
+JOIN Admissions a ON p.patient_id = a.patient_id
+GROUP BY p.patient_id;
