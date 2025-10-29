06ec21691a56a3e3090159600cdd51ed49a562d2
pengukev <130113455+PenguKev@users.noreply.github.com>
2025-10-28 22:58:11 -0400

    Update 3.17.sql

--- queries/3.17.sql
+++ queries/3.17.sql
@@ -1,6 +1,6 @@
 USE HospitalSystem;
 
-SELECT DISTINCT *
+SELECT DISTINCT p.*
 FROM Patients p
 LEFT JOIN NoteEvents n on n.patient_id=p.patient_id
 WHERE note_type="Radiology" AND upper(note_text) like "%chest%"
