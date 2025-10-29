b596b32675f52be823deada4b39e238856381e2e
Yifu <lyf19980216@gmail.com>
2025-10-29 00:36:35 -0400

    Add files including SQL tasks 3.5, 3.8, 3.10, and 3.16

---
+++ queries/3.16.sql
@@ -0,0 +1,8 @@
+-- Task 3.16: List patients who had at least one radiology report during admission
+-- Note: Radiology reports are identified by note_type containing 'Radiology'
+SELECT DISTINCT a.patient_id
+FROM Admissions AS a
+JOIN NoteEvents AS n
+  ON n.admission_id = a.admission_id
+WHERE n.note_type LIKE '%Radiology%'
+ORDER BY a.patient_id;
---
+++ queries/3.5.sql
@@ -0,0 +1,5 @@
+-- Task 3-5: List of all the patients who have private insurance
+SELECT DISTINCT a.patient_id
+FROM Admissions AS a
+WHERE a.insurance = 'Private'
+ORDER BY a.patient_id;
